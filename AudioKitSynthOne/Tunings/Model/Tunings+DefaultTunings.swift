bef84eaef989f50f2d968f70fd47c2e38b12d9a6
Carlos Gomez <armarpc@gmail.com>
2021-06-19 22:15:43 -0400

    Commented tunings

--- AudioKitSynthOne/Tunings/Model/Tunings+DefaultTunings.swift
+++ AudioKitSynthOne/Tunings/Model/Tunings+DefaultTunings.swift
@@ -11,6 +11,7 @@ import Foundation
 extension Tunings {
     internal static func defaultTunings() -> [(String, S1TuningCallback)] {
         var retVal = [(String, S1TuningCallback)]()
+        /*
         retVal.append( ("Chain of pure fifths", { return [1, 3, 9, 27, 81, 243, 729, 2_187, 6_561, 19_683, 59_049, 177_147] }) )
 
         // See http://anaphoria.com/harm&Subharm.pdf
@@ -146,7 +147,7 @@ extension Tunings {
 
         // Harry Partch: PARTCH_43.scl Harry Partch's 43-tone pure scale
         retVal.append( ("Partch", { let s: [Double] = [1/1, 81/80, 33/32, 21/20, 16/15, 12/11, 11/10, 10/9, 9/8, 8/7, 7/6, 32/27, 6/5, 11/9, 5/4, 14/11, 9/7, 21/16, 4/3, 27/20, 11/8, 7/5, 10/7, 16/11, 40/27, 3/2, 32/21, 14/9, 11/7, 8/5, 18/11, 5/3, 27/16, 12/7, 7/4, 16/9, 9/5, 20/11, 11/6, 15/8, 40/21, 64/33, 160/81]; return s } ) )
-
+        */
         // ET
         retVal.append( ("Equal Temperament", { let t = AKTuningTable(); _ = t.equalTemperament(notesPerOctave: 7); return t.masterSet }) )
         retVal.append( ("Equal Temperament", { let t = AKTuningTable(); _ = t.equalTemperament(notesPerOctave: 31); return t.masterSet }) )
